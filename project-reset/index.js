45621fa67756440ce3bf346b81f92e1dfa8aec83
Costas Ioannou <ioannou.dev@gmail.com>
2021-10-28 10:31:18 +0300

    first attempt

---
+++ project-reset/index.js
@@ -0,0 +1,20 @@
+module.exports = function task({ projectName }) {
+  console.log('Hello world')
+  // const { json } = require("mrm-core");
+  // const file = json("package.json");
+  // file.set("name", projectName);
+  // file.set("repository", "");
+  // file.set("author", "");
+  // file.set("description", "");
+  // file.set("version", "0.0.1");
+  // file.save();
+};
+
+module.exports.parameters = {
+  name: {
+    // input, number, confirm, list, rawlist, expand, checkbox, password, editor
+    type: "input",
+    message: "New name for the project",
+  },
+};
+module.exports.description = "Customize project package.json name";
