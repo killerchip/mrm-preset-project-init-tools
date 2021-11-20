bf80d0c0b0f1927aa517140145372c52ba5c1b03
Costas Ioannou <ioannou.dev@gmail.com>
2021-11-20 14:03:00 +0200

    hotfix: clean up redundant file

--- plop/plopfile.ts
+++
@@ -1,9 +0,0 @@
-import { NodePlopAPI } from 'plop';
-
-import plopCommand from './plops/plopCommand';
-//--plop imports--
-
-export default function (plop: NodePlopAPI) {
-  plopCommand(plop);
-  //--plop commands--
-}
