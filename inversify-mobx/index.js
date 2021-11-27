0c63efa1b8db04cc2c9ea1f2bbc2e23521011148
Costas Ioannou <ioannou.dev@gmail.com>
2021-11-27 18:06:12 +0200

    independent task

--- inversify-mobx/index.js
+++ inversify-mobx/index.js
@@ -1,67 +1,7 @@
-const appTsxContent = `import 'reflect-metadata';
-import './src/config/inversify/inversify';`;
+const inversifyMobx = require("./task");
 
 module.exports = function task() {
-  const execSync = require("child_process").execSync;
-  const { install, json, makeDirs } = require("mrm-core");
-  const {
-    installDependencies,
-    copyFile,
-    prependLinesToFile,
-    fileExists,
-    replaceLine,
-  } = require("../utils");
-
-  installDependencies([
-    "inversify",
-    "reflect-metadata",
-    "mobx",
-    "mobx-react-lite",
-  ])
-
-  // config TsConfig
-  const tsConfigFile = json("tsconfig.json");
-  const tsConfigTypes = tsConfigFile.get("compilerOptions.types") || [];
-  const tsConfigNewContent = {
-    compilerOptions: {
-      types: tsConfigTypes.concat(["reflect-metadata"]),
-      experimentalDecorators: true,
-      emitDecoratorMetadata: true,
-      useDefineForClassFields: true,
-    },
-  };
-  tsConfigFile.merge(tsConfigNewContent).save();
-
-  // Create inversify file
-  copyFile(require.resolve("./inversify.ts"), "./src/config/inversify.ts");
-
-  // import necessary stuff
-  prependLinesToFile(appTsxContent, "./App.tsx");
-
-  if (!fileExists("plopfile.ts")) {
-    execSync("mrm plop --preset project-init-tools");
-  }
-
-  // add plop command
-  makeDirs("./plops/addStore");
-  copyFile(require.resolve("./plopIndex.ts"), "./plops/addStore/index.ts");
-
-  copyFile(require.resolve("./store.ts.hbs"), "./plops/addStore/store.ts.hbs");
-
-  // Modify plopfile
-  replaceLine(
-    "//--plop imports--",
-    `import addStore from './plops/addStore'
-    //--plop imports--`,
-    "plopfile.ts"
-  );
-
-  replaceLine(
-    "//--plop commands--",
-    `addStore(plop)
-    //--plop commands--`,
-    "plopfile.ts"
-  );
+  inversifyMobx();
 };
 
 module.exports.description = "Installs and configures inversify";
--- plop/index.js
+++ plop/index.js
@@ -1,50 +1,7 @@
-const { copyFile, installDevDependencies, addLinesToFile } = require("../utils");
-
-const devPackages = ["plop"];
-const prettierIgnore = ["**/*.hbs"];
-
-const commandsRootPath = "plops";
-
-const plopConfigSourcePath = require.resolve("./plopfile.ts");
-const plopConfigTargetPath = "plopfile.ts";
-
-const commandIndexSourcePath = require.resolve("./plopCommand/index.ts");
-const commandTargetPath = commandsRootPath + "/plopCommand";
-const commandIndexTargetFilePath = commandTargetPath + "/index.ts";
-
-const commandIndexHbsSourcePath = require.resolve("./plopCommand/cmdIndex.hbs");
-const commandIndexHbsTargetFilePath = commandTargetPath + "/cmdIndex.hbs";
-
-const commandGeneratedSourcePath = require.resolve(
-  "./plopCommand/cmdGenerated.hbs"
-);
-const commandGeneratedTargetFilePath = commandTargetPath + "/cmdGenerated.hbs";
+const plop = require("./task");
 
 module.exports = function task() {
-  const { makeDirs } = require("mrm-core");
-
-  // Install Plop package
-  installDevDependencies(devPackages);
-
-  // Add Prettier Ignore file
-  addLinesToFile(prettierIgnore, '.prettierIgnore', true)
-
-  // Base plopfile config
-  copyFile(plopConfigSourcePath, plopConfigTargetPath);
-
-  // Plop Command
-  makeDirs(commandsRootPath);
-  makeDirs(commandTargetPath);
-
-  // Plop Command index.ts
-  copyFile(commandIndexSourcePath, commandIndexTargetFilePath);
-
-  // Plop Command plopCommandIndex.hbs
-  copyFile(commandIndexHbsSourcePath, commandIndexHbsTargetFilePath);
-
-  // Plop Command plopCommandHbs.hbs
-  copyFile(commandGeneratedSourcePath, commandGeneratedTargetFilePath);
+  plop();
 };
 
 module.exports.description = "Add plop package (code generator tool)";
-
