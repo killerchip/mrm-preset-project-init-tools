86de43a5bf51197358ab8eb69ece4817ba62b751
Costas Ioannou <ioannou.dev@gmail.com>
2021-11-27 15:31:31 +0200

    clean up

--- inversify-mobx/index.js
+++ inversify-mobx/index.js
@@ -3,15 +3,21 @@ import './src/config/inversify/inversify';`;
 
 module.exports = function task() {
   const execSync = require("child_process").execSync;
-  const { lines, install, json, makeDirs } = require("mrm-core");
+  const { install, json, makeDirs } = require("mrm-core");
   const {
+    installDependencies,
     copyFile,
     prependLinesToFile,
     fileExists,
     replaceLine,
   } = require("../utils");
 
-  install(dependencies, { yarn: true, dev: false });
+  installDependencies([
+    "inversify",
+    "reflect-metadata",
+    "mobx",
+    "mobx-react-lite",
+  ])
 
   // config TsConfig
   const tsConfigFile = json("tsconfig.json");
@@ -59,10 +65,3 @@ module.exports = function task() {
 };
 
 module.exports.description = "Installs and configures inversify";
-
-const dependencies = [
-  "inversify",
-  "reflect-metadata",
-  "mobx",
-  "mobx-react-lite",
-];
